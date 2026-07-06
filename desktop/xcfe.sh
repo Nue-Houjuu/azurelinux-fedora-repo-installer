30b1a579edef118baa6f75dcf900b284dd6f4860
Nue-Houjuu <Dany739264@proton.me>
2026-07-05 22:40:31 -0600

    Update xcfe.sh

--- desktop/xcfe.sh
+++ desktop/xcfe.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/env bash
 
+echo
+echo "Installing XFCE..."
+echo
+
+dnf group install xfce-desktop -y
+
+dnf install lightdm lightdm-gtk -y
+
+systemctl enable lightdm
+
+systemctl set-default graphical.target
+
+echo
+echo "XFCE installed successfully!"
